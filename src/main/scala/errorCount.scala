039ef7476cec4cbbbd0d6b47d8589b0be5b4d8bc
Vigneishwar <36326070+vigneishwar@users.noreply.github.com>
2022-09-01 18:37:35 +1000

    word count using parallize method

---
+++ src/main/scala/errorCount.scala
@@ -0,0 +1,19 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+
+
+object errorCount extends App {
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  val sc = new SparkContext("local[*]","errorcount")
+  // creating a list locally and distributiing it as RDD using parallize method
+  val myList = List("WARN: xxxxx", "ERROR: xxxxx", "ERROR: xxxxx", "ERROR: xxxxx", "ERROR: xxxxx", "ERROR: xxxxx")
+  val originalLogsRDD = sc.parallelize(myList)
+  val mappedColumns = originalLogsRDD.map(x => {
+    val columns = x.split(";")
+    val cols = columns(0)
+    (cols,1)
+  })
+  val results = mappedColumns.reduceByKey((x,y) => x+y)
+  results.collect().foreach(println)
+
+}
