802584eb30c3f0016118819386b0bc6ac11c4c7f
Vigneishwar <36326070+vigneishwar@users.noreply.github.com>
2022-09-04 15:05:48 +1000

    using sparkConf

--- src/main/scala/dataframeExample.scala
+++ src/main/scala/dataframeExample.scala
@@ -1,11 +1,17 @@
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 object dataframeExample extends  App {
 
+  val sparkConf = new SparkConf()
+  sparkConf.set("spark.conf.name", "dataframe example")
+  sparkConf.set("spark.master", "local[2]")
+
   val spark = SparkSession.builder()
-    .appName("Dataframe Example")
-    .master("local[2]")
-    .getOrCreate()
+    .config(sparkConf)
+//    .appName("Dataframe Example")
+//    .master("local[2]")
+    .getOrCreate() // spark session is singelton i.e one spark session for one application
 
   spark.stop()
 
