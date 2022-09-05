c60c911d4c5ec4ad20cdb35c115d26008cb15460
Vigneishwar <36326070+vigneishwar@users.noreply.github.com>
2022-09-05 10:13:29 +1000

    simpler way to load data

--- src/main/scala/orderDF.scala
+++ src/main/scala/orderDF.scala
@@ -13,20 +13,30 @@ object orderDF extends App {
   val spark = SparkSession.builder()
     .config(sparkConf)
     .getOrCreate()
+//
+//  val input = spark.read
+//    .format("csv")
+//    .option("header", true)
+//    .option("inferSchema", true) // never use inferSchema in production
+//    .option("path", "/Users/vigneishn/Downloads/orders-201019-002101.csv")
+//    .load
+//    .csv("/Users/vigneishn/Downloads/orders-201019-002101.csv")
+
 
   val input = spark.read
+    .format("json")
     .option("header", true)
-    .option("inferSchema", true) // never use inferSchema in production
-    .csv("/Users/vigneishn/Downloads/orders-201019-002101.csv")
+    .option("path", "/Users/vigneishn/Downloads/players-201019-002101.json") // no need of inferSchema as JSON files infer the schema by default
+    .load
+//
+//  val groupedOrders = input
+//    .repartition(4)
+//    .where("order_customer_id > 10000")
+//    .select("order_id", "order_customer_id")
+//    .groupBy("order_customer_id")
+//    .count()
 
-  val groupedOrders = input
-    .repartition(4)
-    .where("order_customer_id > 10000")
-    .select("order_id", "order_customer_id")
-    .groupBy("order_customer_id")
-    .count()
-
-  groupedOrders.show()
+  input.show()
   scala.io.StdIn.readLine()
   spark.stop()
 
