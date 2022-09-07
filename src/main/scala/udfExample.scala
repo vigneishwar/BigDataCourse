cc242196980521cdb7059dfde259941c6ef13e60
Vigneishwar <36326070+vigneishwar@users.noreply.github.com>
2022-09-07 14:16:11 +1000

    Spark UDF

--- src/main/scala/udfExample.scala
+++ src/main/scala/udfExample.scala
@@ -1,10 +1,15 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions.{col, udf}
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
 
 object udfExample extends App {
   Logger.getLogger("org").setLevel(Level.ERROR)
+
+  def ageCheck(age: Int) = {
+    if (age>18) "Y" else "N"
+  }
 
   val sparkConf = new SparkConf()
   sparkConf.set("spark.app.name", "UDF in spark")
@@ -17,13 +22,15 @@ object udfExample extends App {
   val input = spark.read
     .format("csv")
     .option("inferSchema", true)
-    .option("path", "/Users/vigneishn/Downloads/-201025-223502 (1).dataset1")
+    .option("path", "/Users/vigneishn/Downloads/dataset1.csv")
     .load()
 
   val namedInput = input.toDF("name", "age", "city")
-  namedInput.printSchema()
+
+  val parseAgeFunc = udf(ageCheck(_:Int): String)
+  val adultCol = namedInput.withColumn("adult", parseAgeFunc(col("age"))) // use .withColumn to add a new column
+  adultCol.printSchema()
+  adultCol.show()
 
   spark.stop()
-
-
 }
